f59989382799648072e3437d8d485f3cef31ca4a
Chema <neo22s@gmaill.com>
2013-11-05 13:31:07 +0100

    empty data folder

--- install/functions.php
+++ install/functions.php
@@ -267,7 +267,7 @@ function hostingAd()
         <p>We have partnership with hosting companies to assure compatibility. And we include:
             <ul>
                 <li>100% Compatible High Speed Hosting</li>
-                <li>1 Premium Theme, of your choice worth $69.99</li>
+                <li>1 Premium Theme, of your choice worth $99.99</li>
                 <li>Professional Installation and Support worth $89</li>
                 <li>Free Domain name, worth $10</li>
             <a class="btn btn-primary btn-large" href="http://open-classifieds.com/hosting/">
