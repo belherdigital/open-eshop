5e0e5dffa33b00e0c863e08ad6870e68bfb236d7
Chema <neo22s@gmaill.com>
2013-11-13 12:27:50 +0100

    closed #66

--- themes/default/views/oc-panel/header.php
+++ themes/default/views/oc-panel/header.php
@@ -27,7 +27,7 @@
             	    <li  class="dropdown "><a href="#" class="dropdown-toggle"
             		      data-toggle="dropdown"><i class="icon-plus icon-white"></i> <?=__('New')?> <b class="caret"></b></a>
                     	<ul class="dropdown-menu">
-                            
+                            <?=Theme::admin_link(__('Bog post'),'blog','create')?>
                             <?=Theme::admin_link(__('Product'),'product','create')?>
                             <?=Theme::admin_link(__('Category'),'category','create')?>
                             <?=Theme::admin_link(__('Page'),'content','create')?>
