f7044ae10bd30a6886a60e7488ba9636747e0a50
chema <neo22s@gmail.com>
2013-12-28 12:15:53 +0100

    support improvement

--- oc/classes/controller/panel/support.php
+++ oc/classes/controller/panel/support.php
@@ -45,6 +45,7 @@ class Controller_Panel_Support extends Auth_Controller {
 
         $tickets = $tickets->where('id_ticket_parent', 'IS', NULL)
                         ->order_by('status','asc')
+                        ->order_by('created','desc')
                         ->find_all();
 
         $this->template->bind('content', $content);
