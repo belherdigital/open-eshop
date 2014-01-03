d3fad4a347c0fa9a5b51409f1a38027a79f76e1a
chema <neo22s@gmail.com>
2014-01-03 09:47:45 +0100

    closes #108

--- oc/classes/controller/panel/coupon.php
+++ oc/classes/controller/panel/coupon.php
@@ -12,4 +12,42 @@ class Controller_Panel_Coupon extends Auth_Crud {
 	 */
 	protected $_orm_model = 'coupon';
 
+
+    /**
+     *
+     * Loads a basic list info
+     * @param string $view template to render 
+     */
+    public function action_index($view = NULL)
+    {
+        $this->template->title = __('Coupons');
+        $this->template->scripts['footer'][] = 'js/oc-panel/crud/index.js';
+        
+        $elements = new Model_Coupon();
+        
+        if (core::get('name')!==NULL)
+            $elements = $elements->where('name', '=', core::get('name'));
+
+
+        $pagination = Pagination::factory(array(
+                    'view'           => 'pagination',
+                    'total_items'    => $elements->count_all(),
+        ))->route_params(array(
+                    'controller' => $this->request->controller(),
+                    'action'     => $this->request->action(),
+        ));
+
+        $pagination->title($this->template->title);
+
+        $elements = $elements->order_by('created','desc')
+        ->limit($pagination->items_per_page)
+        ->offset($pagination->offset)
+        ->find_all();
+
+        $pagination = $pagination->render();
+
+        
+        $this->render('oc-panel/pages/coupon/index', array('elements' => $elements,'pagination'=>$pagination));
+    }  
+
 }
