120e6d6b48752799127fe8402671beccf483e613
Slobodan <slobodan.josifovic@gmail.com>
2014-02-05 13:48:33 +0100

    close #147, for the time being because all issues reported are fixed. If something new appear we can reopen

--- themes/default/views/oc-panel/pages/widgets/widget.php
+++ themes/default/views/oc-panel/pages/widgets/widget.php
@@ -1,31 +1,31 @@
 <?php defined('SYSPATH') or die('No direct script access.');?>
-
-    <div class="col-md-8">
-        <div class="page-header">
-            <h2><?=__('Available widgets')?></h2>
-            <a href="http://open-classifieds.com/2013/08/26/overview-of-widgets/" target="_blank"><?=__('Read more')?></a>
-        </div>
-        <?$i=1?>
-        <?foreach ($widgets as $widget):?>
-            <?=$widget->form()?>
-            <?if($i%3 == 0):?><div class="clearfix"></div><?endif?>
-        <?$i++;endforeach?>
-    </div><!--/span--> 
+<div class="page-header">
+    <h2><?=__('Available widgets')?></h2>
+    <a href="http://open-classifieds.com/2013/08/26/overview-of-widgets/" target="_blank"><?=__('Read more')?></a>
+</div>
+<div class="col-md-8">
     
-    <!--placeholders-->
-    <div class="col-md-4">
-        <?foreach ($placeholders as $placeholder=>$widgets):?>
-        <div class="well sidebar-nav">
-        <p class="nav-header"><?=$placeholder?></p>
-            <ul class="nav nav-list plholder" id="<?=$placeholder?>" >
-                <?foreach ($widgets as $widget):?>
-                  <?=$widget->form()?>
-                <?endforeach?>
-            </ul>
-        </div>
-        <?endforeach?>
-        <span id='ajax_result' data-url='<?=Route::url('oc-panel',array('controller'=>'widget','action'=>'saveplaceholders'))?>'></span>
+    <?$i=1?>
+    <?foreach ($widgets as $widget):?>
+        <?=$widget->form()?>
+        <?if($i%3 == 0):?><div class="clearfix"></div><?endif?>
+    <?$i++;endforeach?>
+</div><!--/span--> 
+
+<!--placeholders-->
+<div class="col-md-4">
+    <?foreach ($placeholders as $placeholder=>$widgets):?>
+    <div class="well well-sm">
+    <h4 class=""><?=$placeholder?></h4>
+        <ul class="nav nav-list plholder" id="<?=$placeholder?>" >
+            <?foreach ($widgets as $widget):?>
+              <?=$widget->form()?>
+            <?endforeach?>
+        </ul>
     </div>
-    <!--placeholders-->
+    <?endforeach?>
+    <span id='ajax_result' data-url='<?=Route::url('oc-panel',array('controller'=>'widget','action'=>'saveplaceholders'))?>'></span>
+</div>
+<!--placeholders-->
 
 
