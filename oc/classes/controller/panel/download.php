7109e84de23e5571f4efe7e2167cafdd4953fbce
chema <neo22s@gmail.com>
2014-02-13 19:47:46 +0100

    closes #100

--- oc/classes/controller/panel/download.php
+++ oc/classes/controller/panel/download.php
@@ -12,11 +12,56 @@ class Controller_Panel_Download extends Auth_Crud {
 	/**
 	 * @var $_index_fields ORM fields shown in index
 	 */
-	protected $_index_fields = array('id_download','id_order','id_user','created');
+	protected $_index_fields = array('id_download','id_order','id_user','ip_address','created');
 
 	/**
 	 * @var $_orm_model ORM model name
 	 */
 	protected $_orm_model = 'download';
+
+
+    /**
+     *
+     * Loads a basic list info
+     * @param string $view template to render 
+     */
+    public function action_index($view = NULL)
+    {
+        $this->template->title = __($this->_orm_model);
+        $this->template->scripts['footer'][] = 'js/oc-panel/crud/index.js';
+        
+        $elements = ORM::Factory($this->_orm_model);//->find_all();
+
+
+        //email search
+        if (Valid::email(core::get('email')))
+        {
+            $users = new Model_User();
+            $users->where('email','=',core::get('email'))->limit(1)->find();
+            if ($users->loaded())
+               $elements->where('id_user','=',$users->id_user);
+        }
+
+        $pagination = Pagination::factory(array(
+                    'view'           => 'pagination',
+                    'total_items'    => $elements->count_all(),
+        //'items_per_page' => 10// @todo from config?,
+        ))->route_params(array(
+                    'controller' => $this->request->controller(),
+                    'action'     => $this->request->action(),
+        ));
+
+        $pagination->title($this->template->title);
+
+        $elements = $elements->order_by('created','desc')
+        ->limit($pagination->items_per_page)
+        ->offset($pagination->offset)
+        ->find_all();
+
+        $pagination = $pagination->render();
+
+        
+        $this->render('oc-panel/pages/download/index', array('elements' => $elements,'pagination'=>$pagination));
+    }    
 
 }
---
+++ themes/default/views/oc-panel/pages/download/index.php
@@ -0,0 +1,65 @@
+<?php defined('SYSPATH') or die('No direct script access.');?>
+<form class="form-inline pull-right" method="get" action="<?=URL::current();?>">
+  	<div class="form-group">
+  		<input type="text" class="form-control" name="email" placeholder="<?=__('Email')?>" value="<?=core::get('email')?>">
+  	</div>
+  	<button type="submit" class="btn"><?=__('Search')?></button>
+</form>
+<div class="page-header">
+    
+	<h1><?=ucfirst(__($name))?></h1>
+
+	<?if ($controller->allowed_crud_action('create')):?>
+	<a class="btn btn-primary pull-right" href="<?=Route::url($route, array('controller'=> Request::current()->controller(), 'action'=>'create')) ?>">
+		<i class="glyphicon glyphicon-pencil"></i>
+		<?=__('New')?>
+	</a>				
+	<?endif?>
+</div>
+<div class="table-responsive">
+	<table class="table table-striped table-bordered">
+		<thead>
+			<tr>
+				<?foreach($fields as $field):?>
+					<th><?=ucfirst((method_exists($orm = ORM::Factory($name), 'formo') ? Arr::path($orm->formo(), $field.'.label', __($field)) : __($field)))?></th>
+				<?endforeach?>
+				<?if ($controller->allowed_crud_action('delete') OR $controller->allowed_crud_action('update')):?>
+				<th><?=__('Actions') ?></th>
+				<?endif?>
+			</tr>
+		</thead>
+		<tbody>
+			<?foreach($elements as $element):?>
+				<tr id="tr<?=$element->pk()?>">
+					
+                    <td><?=$element->id_download?></td>
+                    <td><a href="<?=Route::url('oc-panel', array('controller'=> 'order', 'action'=>'update','id'=>$element->order->pk())) ?>">
+                        <?=$element->order->product->title?> - <?=round($element->order->amount,2)?><?=$element->order->currency?> <?=Date::format($element->order->pay_date,'d-m-y')?>
+                    </td>
+                    <td><a href="<?=Route::url('oc-panel', array('controller'=> 'user', 'action'=>'update','id'=>$element->user->pk())) ?>">
+                        <?=$element->user->name?></a> - <?=$element->user->email?>
+                    </td>
+                    <td><a target="_blank" href="http://www.ipgetinfo.com/?mode=ip&lang=en&ip=<?=long2ip($element->ip_address)?>"><?=long2ip($element->ip_address)?></a></td>
+                    <td><?=$element->created?></td>
+					<?if ($controller->allowed_crud_action('delete') OR $controller->allowed_crud_action('update')):?>
+					<td width="80px">
+						<?if ($controller->allowed_crud_action('update')):?>
+						<a title="<?=__('Edit')?>" class="btn btn-primary" href="<?=Route::url($route, array('controller'=> Request::current()->controller(), 'action'=>'update','id'=>$element->pk()))?>">
+							<i class="glyphicon glyphicon-edit"></i>
+						</a>
+						<?endif?>
+						<?if ($controller->allowed_crud_action('delete')):?>
+						<a data-text="<?=__('Are you sure you want to delete?')?>" 
+							data-id="tr<?=$element->pk()?>" class="btn btn-danger index-delete" title="<?=__('Delete')?>" href="<?=Route::url($route, array('controller'=> Request::current()->controller(), 'action'=>'delete','id'=>$element->pk()))?>">
+							<i class="glyphicon glyphicon-trash"></i>
+						</a>
+						<?endif?>
+					</td>
+					<?endif?>
+				</tr>
+			<?endforeach?>
+		</tbody>
+	</table>
+</div>
+
+<?=$pagination?>
