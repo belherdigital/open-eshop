19dd463135b202ed519a878aae0b9ead442a5ade
chema <neo22s@gmail.com>
2014-02-25 16:28:04 +0100

    nav_link

---
+++ themes/default/views/nav_link.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');?>
+<li alt="<?=$route?>" title="<?=$route?>" class="<?=(Request::current()->controller()==$controller AND Request::current()->action()==$action)?'active':''?> <?=$style?>" >
+    <a  href="<?=Route::url($route,array('controller'=>$controller,'action'=>$action,'id'=>$id))?>">
+        <?if($icon!==NULL):?>
+            <i class="<?=$icon?>"></i>
+        <?endif?>
+        <?=$name?>
+    </a>
+</li>
