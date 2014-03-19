78fbeb4bf23ade55d0768e254ad6ddbf6e36beb5
Slobodan <slobodan.josifovic@gmail.com>
2014-03-19 17:23:51 +0100

    widget last reviews #136

---
+++ oc/modules/widgets/classes/widget/reviews.php
@@ -0,0 +1,66 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * reviews widget reader
+ *
+ * @author      Slobodan <slobodan@open-classifieds.com>
+ * @package     Widget
+ * @copyright   (c) 2009-2013 Open Classifieds Team
+ * @license     GPL v3
+ */
+
+
+class Widget_Reviews extends Widget
+{
+
+    public function __construct()
+    {   
+
+        $this->title        = __('Product Reviews');
+        $this->description  = __('Product Reviews reader');
+
+        $this->fields = array(  'reviews_limit' => array(  'type'      => 'text',
+                                                        'display'   => 'text',
+                                                        'label'     => __('Review Limit, if none display all'),
+                                                        'default'   => '',
+                                                        'required'  => FALSE),
+
+                                'review_title'  => array(  'type'      => 'text',
+                                                        'display'   => 'text',
+                                                        'label'     => __('Review title displayed'),
+                                                        'default'   => 'Latest reviews',
+                                                        'required'  => FALSE),
+                                );
+    }
+
+    /**
+     * get the title for the widget
+     * @param string $title we will use it for the loaded widgets
+     * @return string 
+     */
+    public function title($title = NULL)
+    {
+        return parent::title($this->review_title);
+    }
+
+    /**
+     * Automatically executed before the widget action. Can be used to set
+     * class properties, do authorization checks, and execute other custom code.
+     *
+     * @return  void
+     */
+    public function before()
+    {
+        $review = new Model_Review();
+        $review->where('status', '=', Model_Review::STATUS_ACTIVE)
+            ->order_by('created','desc');
+        
+        if($this->reviews_limit != NULL OR $this->reviews_limit != '')
+        $review = $review->limit($this->reviews_limit)->cached()->find_all();
+        else
+        $review = $review->cached()->find_all();
+        //die(print_r($review));
+        $this->review = $review;
+    }
+
+
+}
---
+++ oc/modules/widgets/views/widget_reviews.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die('No direct script access.');?>
+<h3><?=$widget->review_title?></h3>
+<ul>
+
+<?foreach($widget->review as $review):?>
+
+    <div class="media">
+        <a class="pull-left">
+            <img class="media-object dp img-circle" src="<?=$review->user->get_profile_image()?>" data-toggle="tooltip" data-placement="top" title="<?=$review->user->name?>" style="width: 100px;height:100px;">
+        </a>
+        <div class="media-body">
+            <h4 class="media-heading"><a href="<?=Route::url('product', array('category'=>$review->product->category->seoname,'seotitle'=>$review->product->seotitle))?>"><?=Text::limit_chars(Text::bb2html($review->product->title,TRUE),15, NULL, TRUE)?></a><span class="label label-warning"><?=$review->rate?></span></h4>
+            <h5><?=Text::limit_chars(Text::bb2html($review->description,TRUE),30, NULL, TRUE)?></h5>
+            <hr style="margin:8px auto">
+        </div>
+    </div>
+<?endforeach?>
+</ul>
+
+
+
+
+        
+
+
