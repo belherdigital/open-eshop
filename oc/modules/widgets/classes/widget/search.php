a43e1c875d88a1b4dcdbce3ec60fa59bdb97c1cd
Slobodan <slobodan.josifovic@gmail.com>
2014-03-31 14:55:47 +0200

    close #202

---
+++ oc/modules/widgets/classes/widget/search.php
@@ -0,0 +1,84 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Search widget reader
+ *
+ * @author      Slobodan <slobodan@open-classifieds.com>
+ * @package     Widget
+ * @copyright   (c) 2009-2013 Open Classifieds Team
+ * @license     GPL v3
+ */
+
+
+class Widget_Search extends Widget
+{
+
+	public function __construct()
+	{	
+
+		$this->title 		= __('Search');
+		$this->description 	= __('Advanced Search');
+
+        $this->fields = array(	'text_title'    => array( 'type'      => 'text',
+                                                        'display'   => 'text',
+                                                        'default'   => __('Search'),
+                                                        'label'     => __('Title displayed'),
+                                                        'required'  => FALSE),
+
+                                'advanced'      => array(  'type'      => 'text',
+                                                        'display'   => 'select',
+                                                        'label'     => __('Advanced option'),
+                                                        'options'   => array('0'   => __('FALSE'),
+                                                                             '1'   => __('TRUE'),
+                                                                            ), 
+                                                        'default'   => 0,
+                                                        'required'  => TRUE),
+						 		);
+	}
+
+	/**
+     * get the title for the widget
+     * @param string $title we will use it for the loaded widgets
+     * @return string 
+     */
+    public function title($title = NULL)
+    {
+        return parent::title($this->text_title);
+    }
+
+    /**
+     * Automatically executed before the widget action. Can be used to set
+     * class properties, do authorization checks, and execute other custom code.
+     *
+     * @return  void
+     */
+    public function before()
+    {
+
+       
+        // get all categories
+        if ($this->advanced != FALSE)
+        {
+            // loaded category
+            list($categories,$order_categories)  = Model_Category::get_all();
+
+            $arr_cat = array();
+            foreach ($categories as $cat => $value) 
+            {
+                if($value['id'] != 1)
+                    $arr_cat[$value['id']] = $value['name'];
+            }
+            
+            $this->cat_items = $categories;
+            $this->cat_order_items = $order_categories;
+
+        }
+
+        if($this->custom != FALSE)
+        {
+            $fields = Model_Field::get_all();
+            $this->custom_fields = $fields;
+        }
+    }
+
+
+}
---
+++ oc/modules/widgets/views/widget_search.php
@@ -0,0 +1,63 @@
+<?php defined('SYSPATH') or die('No direct script access.');?>
+<h3><?=$widget->text_title?></h3>
+<div>
+<?= FORM::open(Route::url('search'), array('class'=>'form-horizontal', 'method'=>'GET', 'action'=>'','enctype'=>'multipart/form-data'))?>
+<!-- if categories on show selector of categories -->
+    <div class="form-group">
+        <div class="col-xs-10">  
+            <?= FORM::label('product', __('Product Title'), array('class'=>'', 'for'=>'product'))?>
+            <input type="text" id="title" name="title" class="form-control" value="" placeholder="<?=__('Search')?>">
+        </div>
+    </div>
+<?if($widget->advanced != FALSE):?>
+    <?if($widget->cat_items !== NULL):?>
+        <div class="form-group">
+            <div class="col-xs-10">
+                <?= FORM::label('category', __('Categories'), array('class'=>'', 'for'=>'category_widget_search'))?>
+                <select data-placeholder="<?=__('Categories')?>" name="category" id="category_widget_search" class="form-control">
+                <option value="<?=core::request('category')?>"></option>
+                <?function lili_search($item, $key,$cats){?>
+                <?if ( count($item)==0 AND $cats[$key]['id_category_parent'] != 1):?>
+                <option value="<?=$cats[$key]['seoname']?>" data-id="<?=$cats[$key]['id']?>" <?=(core::request('category') == $cats[$key]['seoname'])?"selected":''?> ><?=$cats[$key]['name']?></option>
+                
+                <?endif?>
+                    <?if ($cats[$key]['id_category_parent'] == 1 OR count($item)>0):?>
+                    <option value="<?=$cats[$key]['seoname']?>" <?=(core::request('category') == $cats[$key]['seoname'])?"selected":''?>> <?=$cats[$key]['name']?> </option>
+                        <optgroup label="<?=$cats[$key]['name']?>">  
+                        <? if (is_array($item)) array_walk($item, 'lili_search', $cats);?>
+                        </optgroup>
+                    <?endif?>
+                <?}
+                $cat_order = $widget->cat_order_items; 
+                array_walk($cat_order , 'lili_search', $widget->cat_items);?>
+                </select> 
+            </div>
+        </div>
+    <?endif?>
+<!-- end categories/ -->
+
+    
+        <div class="form-group">
+             
+            <div class="col-xs-10"> 
+                <label class="" for="price-min"><?=__('Price from')?> </label>
+                <input type="text" id="price-min" name="price-min" class="form-control" value="<?=core::get('price-min')?>" placeholder="<?=__('Price from')?>">
+            </div>
+        </div>
+
+        <div class="form-group">
+            
+            <div class="col-xs-10">
+                <label class="" for="price-max"><?=__('Price to')?></label>
+                <input type="text" id="price-max" name="price-max" class="form-control" value="<?=core::get('price-max')?>" placeholder="<?=__('to')?>">
+            </div>
+        </div>
+    
+<?endif?>
+
+<!-- /endcustom fields -->
+<div class="clearfix"></div>
+
+    <?= FORM::button('submit', __('Search'), array('type'=>'submit', 'class'=>'btn btn-primary', 'action'=>Route::url('search')))?> 
+<?= FORM::close()?>
+</div>
