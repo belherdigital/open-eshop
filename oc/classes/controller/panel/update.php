fc1ce9dce30737c71d1af6686bfc842ef4872de9
chema <neo22s@gmail.com>
2014-05-20 23:56:12 +0200

    update kohana no ko323 so later the updates always will work

--- oc/classes/controller/panel/update.php
+++ oc/classes/controller/panel/update.php
@@ -434,7 +434,7 @@ class Controller_Panel_Update extends Auth_Controller {
                           'oc/modules/',
                           'oc/vendor/',
                           'oc/bootstrap.php',
-                          'oc/ko323/',
+                          'oc/kohana/',
                           'themes/',
                           'languages/',
                           'index.php',
