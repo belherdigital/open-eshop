aeb7170b9108c001a8f7c5be415d6737f61855b3
Chema <neo22s@gmail.com>
2014-06-30 13:34:02 +0200

    closes open-classifieds/openclassifieds2#818

--- themes/default/views/main.php
+++ themes/default/views/main.php
@@ -110,12 +110,6 @@
 
 	<?=Theme::scripts($scripts,'footer')?>
 	
-    
-	
-	<!--[if lt IE 7 ]>
-		<?=HTML::script('http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js')?>
-		<script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script>
-	<![endif]-->
   <?=(Kohana::$environment === Kohana::DEVELOPMENT)? View::factory('profiler'):''?>
   </body>
 </html>
