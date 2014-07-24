4ad805a90095137e670aa105742e201e43198fb6
Oliver <oliver@open-classifieds.com>
2014-07-23 22:04:50 -0500

    SCEditor paste as plain text open-classifieds/openclassifieds2#841

--- themes/default/js/forum-new.js
+++ themes/default/js/forum-new.js
@@ -11,4 +11,11 @@ $(function(){
     $("button[name=submit]").click(function(){
         $("textarea[name=description]").data("sceditor").updateTextareaValue();
     });
+
+	// paste plain text in sceditor
+	$(".sceditor-container iframe").contents().find("body").bind('paste', function(e) {
+		e.preventDefault();
+		var text = (e.originalEvent || e).clipboardData.getData('text/plain');
+		$(".sceditor-container iframe")[0].contentWindow.document.execCommand('insertText', false, text);
+	});	
 });
