3501d9a6d86b21d5bf3d0059995aec4db0251d5e
Chema <neo22s@gmail.com>
2014-08-05 10:02:08 +0200

    error gettext location

--- install/class.install.php
+++ install/class.install.php
@@ -280,7 +280,7 @@ class install{
              * We load php-gettext here since Kohana_I18n tries to create the function __() function when we extend it.
              * PHP-gettext already does this.
              */
-            include APPPATH.'modules/common/vendor/php-gettext/gettext.inc';
+            include APPPATH.'common/vendor/php-gettext/gettext.inc';
             
             T_setlocale(LC_ALL, $locale);
             T_bindtextdomain($domain,DOCROOT.'languages');
