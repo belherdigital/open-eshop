d0541fb1de083e64c239d6ecf9e5df692dcabc8d
Chema <neo22s@gmail.com>
2014-08-06 16:57:34 +0200

    closes open-classifieds/openclassifieds2#885

--- oc/classes/controller/panel/auth.php
+++ oc/classes/controller/panel/auth.php
@@ -91,7 +91,9 @@ class Controller_Panel_Auth extends Controller {
                     $url_ql = $user->ql('oc-panel',array( 'controller' => 'profile', 
                                                           'action'     => 'changepass'),TRUE);
 
-                    $ret = $user->email('auth-remember',array('[URL.QL]'=>$url_ql));
+                    //we don't use this since checks if the user is subscribed which is stupid since you want to remember your password.
+                    //$ret = $user->email('auth-remember',array('[URL.QL]'=>$url_ql));
+                    $ret = Email::content($user->email,$user->name,NULL,NULL,'auth-remember',array('[URL.QL]'=>$url_ql)); 
 
                     //email sent notify and redirect him
                     if ($ret)
@@ -116,89 +118,71 @@ class Controller_Panel_Auth extends Controller {
 				
 			
 	}
-	
-	/**
-	 * Simple register for user
-	 *
-	 */
-	public function action_register()
-	{
-		$this->template->content = View::factory('pages/auth/register');
-		$this->template->content->msg = '';
-		
-		//if user loged in redirect home
-		if (Auth::instance()->logged_in())
-		{
-			$this->redirect(Route::get('oc-panel')->uri());
-		}
-		//posting data so try to remember password
-		elseif (core::post('email') AND CSRF::valid('register'))
-		{
-			$email = core::post('email');
-				
-			if (Valid::email($email,TRUE))
-			{
-				
-				//check we have this email in the DB
-				$user = new Model_User();
-				$user = $user->where('email', '=', $email)
-						->limit(1)
-						->find();
-		
-				if ($user->loaded())
-				{
-					Form::set_errors(array(__('User already exists')));
-				}
-				else
-				{
-                    $password           = Text::random('alnum', 8);
-					//create user
-					$user->email 	= $email;
-					$user->name		= core::post('name');
-					$user->status	= Model_User::STATUS_ACTIVE;
-					$user->id_role	= 1;//normal user
-					$user->password = $password;
-					$user->seoname 	= $user->gen_seo_title(core::post('name'));
-					
-					try
-					{
-						$user->save();
-					}
-					catch (ORM_Validation_Exception $e)
-					{
-						//Form::errors($content->errors);
-					}
-					catch (Exception $e)
-					{
-						throw HTTP_Exception::factory(500,$e->getMessage());
-					}
-					
-					//login the user
-					Auth::instance()->login(core::post('email'), $password);
-                    //send email
-                    $user->email('auth-register',array('[USER.PWD]'=>$password,
-                                                        '[URL.QL]'=>$user->ql('default',NULL,TRUE))
-                                                );
 
-                    Alert::set(Alert::SUCCESS, __('Welcome!'));
-                    //login the user
-					$this->redirect(Core::post('auth_redirect',Route::url('oc-panel')));
-					
-				}
-		
-				
-			}
-			else
-			{
-				Form::set_errors(array(__('Invalid Email')));
-			}
-				
-		}
-	
-		//template header
-		$this->template->title            = __('Register new user');
-			
-	}
+    /**
+     * Simple register for user
+     *
+     */
+    public function action_register()
+    {
+        $this->template->content = View::factory('pages/auth/register');
+        $this->template->content->msg = '';
+        
+        //if user loged in redirect home
+        if (Auth::instance()->logged_in())
+        {
+            $this->redirect(Route::get('oc-panel')->uri());
+        }
+        //posting data so try to remember password
+        elseif (core::post('email') AND CSRF::valid('register'))
+        {
+            $email = core::post('email');
+                
+            if (Valid::email($email,TRUE))
+            {
+                if (core::post('password1')==core::post('password2'))
+                {
+                    //check we have this email in the DB
+                    $user = new Model_User();
+                    $user = $user->where('email', '=', $email)
+                            ->limit(1)
+                            ->find();
+            
+                    if ($user->loaded())
+                    {
+                        Form::set_errors(array(__('User already exists')));
+                    }
+                    else
+                    {
+                        //creating the user
+                        $user = Model_User::create_email($email,core::post('name'),core::post('password1'));
+                        
+                        //login the user
+                        Auth::instance()->login(core::post('email'), core::post('password1'));
+                        
+                        Alert::set(Alert::SUCCESS, __('Welcome!'));
+                        //login the user
+                        $this->redirect(Core::post('auth_redirect',Route::url('oc-panel')));
+                        
+                    }
+        
+                }
+                else
+                {
+                    Form::set_errors(array(__('Passwords do not match')));
+                }
+            }
+            else
+            {
+                Form::set_errors(array(__('Invalid Email')));
+            }
+                
+        }
+    
+        //template header
+        $this->template->title            = __('Register new user');
+            
+    }
     
 	/**
 	 *
