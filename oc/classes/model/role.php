e0a373de65486ebb0d1f6988d1c42ce2a6601308
Chema <neo22s@gmail.com>
2014-08-25 10:05:13 +0200

    fix open-classifieds/openclassifieds2#889

---
+++ oc/classes/model/role.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * User Roles
+ *
+ * @author      Chema <chema@open-classifieds.com>
+ * @package     Core
+ * @copyright   (c) 2009-2014 Open Classifieds Team
+ * @license     GPL v3
+ */
+
+class Model_Role extends Model_OC_Role {}
