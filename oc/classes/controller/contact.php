69b16947fda638ea2f71ccbce766875bbc65608f
Chema <neo22s@gmail.com>
2014-09-15 17:00:36 +0200

    contact form fixed

--- oc/classes/controller/contact.php
+++ oc/classes/controller/contact.php
@@ -51,7 +51,7 @@ class Controller_Contact extends Controller {
                 }
             }
             else
-                Alert::set(Alert::ERROR, __('Check the form for errors'));
+                Alert::set(Alert::ERROR, __('Wrong captcha'));
                     
                 
         }
@@ -59,52 +59,5 @@ class Controller_Contact extends Controller {
         $this->template->content = View::factory('pages/contact');
         
     }
-	public function action_index2()
-	{ 
-
-		//template header
-		$this->template->title           	= __('Contact Us');
-		$this->template->meta_description	= __('Contact').' '.Core::config('general.site_name');
-
-		Breadcrumbs::add(Breadcrumb::factory()->set_title(__('Home'))->set_url(Route::url('default')));
-		Breadcrumbs::add(Breadcrumb::factory()->set_title(__('Contact Us')));
-
-		if($this->request->post()) //message submition  
-		{
-            //captcha check
-            if(captcha::check('contact'))
-            {
-                //akismet spam filter
-                if(!core::akismet(core::post('name'), 
-                                  core::post('email'),
-                                  core::post('message')))
-                {
-                    $replace = array('[EMAIL.BODY]'     =>core::post('message'),
-                                      '[EMAIL.SENDER]'  =>core::post('name'),
-                                      '[EMAIL.FROM]'    =>core::post('email'));
-
-                    if (Email::content(core::config('email.notify_email'),
-                                        core::config('general.site_name'),
-                                        core::post('email'),
-                                        core::post('name'),'contact-admin',
-                                        $replace))
-                        Alert::set(Alert::SUCCESS, __('Your message has been sent'));
-                    else
-                        Alert::set(Alert::ERROR, __('Message not sent'));
-                }
-                else
-                {
-                    Alert::set(Alert::SUCCESS, __('This email has been considered as spam! We are sorry but we can not send this email.'));
-                }
-            }
-            else
-                Alert::set(Alert::ERROR, __('Check the form for errors'));
-					
-				
-		}
-
-        $this->template->content = View::factory('pages/contact');
-		
-	}
 
 }
