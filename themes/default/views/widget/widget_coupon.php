015445216580cf81c4cc8edbe5e0c99c45838560
Oliver <oliverds@users.noreply.github.com>
2014-10-21 19:49:07 -0500

    Improve coupon widget #329

--- themes/default/views/widget/widget_coupon.php
+++ themes/default/views/widget/widget_coupon.php
@@ -1,2 +1,23 @@
 <h3><?=$widget->text_title?></h3>
-<?=View::factory('coupon')?>
+
+<form class=""  method="post" action="<?=URL::current()?>">         
+    <?if (Model_Coupon::current()->loaded()):?>
+        <?=Form::hidden('coupon_delete',Model_Coupon::current()->name)?>
+        <button type="submit" class="btn btn-warning"><?=__('Delete')?> <?=Model_Coupon::current()->name?></button>
+        <p>
+            <?=__('Discount off')?> <?=(Model_Coupon::current()->discount_amount==0)?round(Model_Coupon::current()->discount_percentage,0).'%':round(Model_Coupon::current()->discount_amount,0)?> <br>
+            <?=Model_Coupon::current()->number_coupons?> <?=__('coupons left')?>, <?=__('valid until')?> <?=Date::format(Model_Coupon::current()->valid_date)?>.
+            <?if(Model_Coupon::current()->id_product!=NULL):?>
+                <?=__('only valid for')?>  <a target="_blank" href="<?=Route::url('product', array('seotitle'=>Model_Coupon::current()->product->seotitle,'category'=>Model_Coupon::current()->product->category->seoname)) ?>">
+                        <?=Model_Coupon::current()->product->title;?></a>.
+            <?endif?>
+        </p>
+    <?else:?>
+    <div class="input-group">
+        <input class="form-control" type="text" name="coupon" value="<?=Core::get('coupon')?><?=Core::get('coupon')?>" placeholder="<?=__('Coupon Name')?>">
+        <span class="input-group-btn">
+            <button type="submit" class="btn btn-primary"><?=__('Add')?></button>
+        </span>
+    </div>
+    <?endif?>      	
+</form>
