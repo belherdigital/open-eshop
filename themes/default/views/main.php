324e0e7d537b8b14eef7f74d46cf587718cb4456
chema <neo22s@gmail.com>
2014-11-03 17:09:11 +0100

    analytcis code

--- themes/default/views/main.php
+++ themes/default/views/main.php
@@ -52,20 +52,7 @@
 	<?=core::config('general.html_head')?>
 
     <link rel="shortcut icon" href="<?=core::config('general.base_url').'images/favicon.ico'?>">
-    <?if ( Kohana::$environment === Kohana::PRODUCTION AND core::config('general.analytics')!=='' ): ?>
-    <script type="text/javascript">
-      var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', '<?=Core::config('general.analytics')?>']);
-      _gaq.push(['_setDomainName', '<?=$_SERVER['SERVER_NAME']?>']);
-      _gaq.push(['_setAllowLinker', true]);
-      _gaq.push(['_trackPageview']);
-      (function() {
-        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();
-    </script> 
-    <?endif?>
+    <?=View::factory('analytics')?>
     </head>
 
     <body data-spy="scroll" data-target=".subnav" data-offset="50" class="<?=((Request::current()->controller()!=='faq') AND Theme::get('fixed_toolbar')==1)?'':'body_fixed'?>">
