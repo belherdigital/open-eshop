27f02d49940d5b292820a5d66aa66d82ab1dbd10
Oliver <oliverds@users.noreply.github.com>
2014-11-13 19:08:45 -0600

    Bootswatch 3.2.0 -> 3.3.1+1

--- themes/default/init.php
+++ themes/default/init.php
@@ -27,8 +27,8 @@ Theme::load();
 
 //local files
     $theme_css = array(
-                        '//netdna.bootstrapcdn.com/bootswatch/3.2.0/yeti/bootstrap.min.css' => 'screen',
                         '//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css' => 'screen',
+                        '//netdna.bootstrapcdn.com/bootswatch/3.3.0/yeti/bootstrap.min.css' => 'screen',
                         '//cdn.jsdelivr.net/chosen/1.0.0/chosen.css' => 'screen',
                         '//cdn.jsdelivr.net/prettyphoto/3.1.5/css/prettyPhoto.css' => 'screen',
                         'css/style.css?v='.Core::VERSION => 'screen',
