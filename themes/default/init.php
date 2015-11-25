8ba7a74e9361184d71632a19b5c555b2f5b7aa79
Oliver <oliverds@users.noreply.github.com>
2015-11-25 09:00:24 -0600

    Fontawesome –> 4.5.0

--- themes/default/init.php
+++ themes/default/init.php
@@ -28,7 +28,7 @@ Theme::load();
 //local files
     $theme_css = array(
                         '//netdna.bootstrapcdn.com/bootswatch/3.3.4/yeti/bootstrap.min.css' => 'screen',
-                        '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' => 'screen',
+                        '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css' => 'screen',
                         '//cdn.jsdelivr.net/chosen/1.0.0/chosen.css' => 'screen',
                         '//cdn.jsdelivr.net/prettyphoto/3.1.5/css/prettyPhoto.css' => 'screen',
                         'css/style.css?v='.Core::VERSION => 'screen',
