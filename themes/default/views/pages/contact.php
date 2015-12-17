bf9385917454aa2f800bf7339d2d40f2330c53c1
Oliver <oliverds@users.noreply.github.com>
2015-12-16 20:00:19 -0600

    closes open-classifieds/open-eshop#459: Captcha - reCaptcha

--- themes/default/views/pages/contact.php
+++ themes/default/views/pages/contact.php
@@ -33,13 +33,20 @@
 				</div>
 		</div>
 		
-		<div class="form-group">
-			<div class="col-md-5 col-md-offset-2">
-				<?=__('Captcha')?>*:<br />
-				<?=captcha::image_tag('contact')?><br />
-				<?= FORM::input('captcha', "", array('class' => 'form-control', 'id' => 'captcha', 'required'))?>
+		<?if (core::config('general.captcha') != FALSE):?>
+			<div class="form-group">
+				<div class="col-md-5 col-md-offset-2">
+					<?if (Core::config('general.recaptcha_active')):?>
+						<?=Captcha::recaptcha_display()?>
+	                    <div id="recaptcha1"></div>
+	                <?else:?>
+						<?=__('Captcha')?>*:<br />
+						<?=captcha::image_tag('contact')?><br />
+						<?= FORM::input('captcha', "", array('class' => 'form-control', 'id' => 'captcha', 'required'))?>
+					<?endif?>
+				</div>
 			</div>
-		</div>
+		<?endif?>
 		<br>
 		<div class="form-group">
 			<div class="col-md-5 col-md-offset-2">
