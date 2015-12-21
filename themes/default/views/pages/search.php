8728571a2e6553b287c3057879c24b7cbb06a5ef
Oliver <oliverds@users.noreply.github.com>
2015-12-21 12:33:50 -0600

    Improved bootstrap search form

--- themes/default/views/pages/search.php
+++ themes/default/views/pages/search.php
@@ -3,39 +3,43 @@
 <div class="well clearfix">
     <h1><?=__('Advanced Search')?></h1>
     <?= FORM::open(Route::url('search'), array('class'=>'form-search', 'method'=>'GET', 'action'=>''))?>
-        <div class="">
-            <div class="col-md-3 pl-0">
-                <input type="text" id="search" name="search" class="form-control" value="<?=HTML::chars(core::get('search'))?>" placeholder="<?=__('Name')?>">  
+        <div class="row">
+            <div class="">
+                <div class="col-md-3 pl-0">
+                    <input type="text" id="search" name="search" class="form-control" value="<?=HTML::chars(core::get('search'))?>" placeholder="<?=__('Name')?>">  
+                </div>
             </div>
+            <div class="">
+                <div class="col-md-3 pl-0">
+                    <select name="category" id="category" class="form-control remove_chzn" >
+                    <option><?=__('Category')?></option>
+                    <?function lili($item, $key,$cats){?>
+                    <option value="<?=$cats[$key]['seoname']?>" <?=(core::get('category')==$cats[$key]['seoname']?'selected':'')?> >
+                        <?=$cats[$key]['name']?></option>
+                        <?if (count($item)>0):?>
+                        <optgroup label="<?=HTML::chars($cats[$key]['name'])?>">    
+                            <? if (is_array($item)) array_walk($item, 'lili', $cats);?>
+                        <?endif?>
+                    <?}array_walk($order_categories, 'lili',$categories);?>
+                    </select>
+                </div>
+            </div>
+            <div class="">
+                <div class="col-md-2 pl-0">
+                    <input type="text" id="price-min" name="price-min" class="form-control" value="<?=HTML::chars(core::get('price-min'))?>" placeholder="<?=__('Price from')?>">
+                </div>
+            </div>   
+            <div class="">
+                <div class="col-md-2 pl-0">
+                    <input type="text" id="price-max" name="price-max" class="form-control" value="<?=HTML::chars(core::get('price-max'))?>" placeholder="<?=__('to')?>">
+                </div>
+            </div>
+            <div class="">
+                <div class="col-md-2 pl-0">
+                    <?= FORM::button('submit', __('Search'), array('type'=>'submit', 'class'=>'btn btn-primary', 'action'=>Route::url('search')))?> 
+                </div>
+            </div> 
         </div>
-        <div class="">
-            <div class="col-md-3 pl-0">
-                <select name="category" id="category" class="form-control remove_chzn" >
-                <option><?=__('Category')?></option>
-                <?function lili($item, $key,$cats){?>
-                <option value="<?=$cats[$key]['seoname']?>" <?=(core::get('category')==$cats[$key]['seoname']?'selected':'')?> >
-                    <?=$cats[$key]['name']?></option>
-                    <?if (count($item)>0):?>
-                    <optgroup label="<?=HTML::chars($cats[$key]['name'])?>">    
-                        <? if (is_array($item)) array_walk($item, 'lili', $cats);?>
-                    <?endif?>
-                <?}array_walk($order_categories, 'lili',$categories);?>
-                </select>
-            </div>
-        </div>
-        <div class="">
-            <div class="col-md-2 pl-0">
-                <input type="text" id="price-min" name="price-min" class="form-control" value="<?=HTML::chars(core::get('price-min'))?>" placeholder="<?=__('Price from')?>">
-            </div>
-        </div>   
-        <div class="">
-            <div class="col-md-2 pl-0">
-                <input type="text" id="price-max" name="price-max" class="form-control" value="<?=HTML::chars(core::get('price-max'))?>" placeholder="<?=__('to')?>">
-            </div>
-        </div>
-        <div class="adv-btn">
-            <?= FORM::button('submit', __('Search'), array('type'=>'submit', 'class'=>'btn btn-primary pull-right', 'action'=>Route::url('search')))?> 
-        </div> 
         
 
     <?= FORM::close()?>
