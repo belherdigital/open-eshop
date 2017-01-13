8f540c0e631c18942cf666946a88f7fb7a542799
g4m3r0 <g4m3r009@hotmail.de>
2017-01-13 19:34:16 +0100

    Fix redirection after adding/deleting coupon
    	Fix redirection after adding/deleting coupon

--- themes/default/views/pages/product/checkout.php
+++ themes/default/views/pages/product/checkout.php
@@ -135,7 +135,7 @@
                             </table>
                         </div><!--//col-*-->
                         <div class="col-xs-4">
-                            <form class="form-inline"  method="post" action="<?=Core::get('current_url')?>">         
+                            <form class="form-inline"  method="post" action="<?=URL::site(Request::detect_uri(), TRUE)?>">         
                                 <?if ($order->coupon->loaded()):?>
                                     <?=Form::hidden('coupon_delete',$order->coupon->name)?>
                                     <button type="submit" class="btn btn-xs btn-danger">
